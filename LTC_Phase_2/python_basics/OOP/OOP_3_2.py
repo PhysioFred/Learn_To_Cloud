17405069acc8faee2dd74131ce512f843e4c2add
physiofred <codespace3166@gmail.com>
2024-08-09 18:09:03 +1000

    continue with OOP learning

--- LTC_Phase_2/python_basics/OOP/OOP_3_2.py
+++ LTC_Phase_2/python_basics/OOP/OOP_3_2.py
@@ -5,6 +5,10 @@
 #init meth runs everytime you call the class, thus can make counter
 
 
+#classmethods and staticmethods
+#alternative constructor
+#
+
 class employee_info:
     #class variable
     raise_amount = 1.04
@@ -23,22 +27,39 @@ class employee_info:
         return "{} {}".format(self.first, self.last)
     
     def apply_raise(self):
-        self.pay = int(self.pay * employee_info.raise_amount)
+        self.pay = int(self.pay * self.raise_amount) #changed self.raise_amount (instance) from employee_info.raise_amount (class)
     
     # #hardcoded the raise amount, which is not what we want.
     # def apply_raise(self):
     #     self.pay = int(self.pay * 1.04)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        employee_info.set_raise_amt = amount
 
-print(f"employee count = {employee_info.employee_count}")   
+
+print(f"employee count = {employee_info.employee_count}") 
+print(f"employee standard raise amount {employee_info.raise_amount}")  
 print()
 
 emp_1=employee_info("Fred", "Dejrangsi", 66000)
 emp_2=employee_info("Matilda", "May", 100000)
 
+emp_1.raise_amount = 1.06
+
+print("--------------------")
+print(emp_1.__dict__)
+print("--------------------")
+
+print(f"employee 1 raise amount {emp_1.raise_amount}")
 print(f"pay before raise {emp_1.pay}")
 emp_1.apply_raise()
 print(f"pay after a raise {emp_1.pay}")
+print()
+
+print(f"employee 1 raise amount {emp_2.raise_amount}")
+print(f"pay before raise {emp_2.pay}")
+print(f"pay after a raise {emp_2.pay}")
 
 
 print()
@@ -46,15 +67,16 @@ print(f"employee count = {employee_info.employee_count}")
 print()
 
 #used to find more information on instance or class variables
-print("--------------------")
-print(emp_1.__dict__)
-print("--------------------")
+
 print(employee_info.__dict__)
 
 #trying to use method to make a raise to emp_1
 # print(emp_1.pay)
 # emp_1.apply_raise()
 # print(emp_1.pay)
+
+
+            
 
 
             
--- LTC_Phase_2/python_basics/OOP/OOP_3_3.py
+++ LTC_Phase_2/python_basics/OOP/OOP_3_3.py
@@ -1,9 +1,5 @@
-#class variables vs instance variables
-#__dict__
-#vocab: hardcode, namespace, instantiated
-#instance amount can change vs class amount is global
-#init meth runs everytime you call the class, thus can make counter
-
+#classmethods and staticmethods
+#alternative constructor
 
 class employee_info:
     #class variable
@@ -23,38 +19,71 @@ class employee_info:
         return "{} {}".format(self.first, self.last)
     
     def apply_raise(self):
-        self.pay = int(self.pay * employee_info.raise_amount)
+        self.pay = int(self.pay * self.raise_amount) #changed self.raise_amount (instance) from employee_info.raise_amount (class)
     
     # #hardcoded the raise amount, which is not what we want.
     # def apply_raise(self):
     #     self.pay = int(self.pay * 1.04)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        employee_info.raise_amount = amount
 
-print(f"employee count = {employee_info.employee_count}")   
+#employee count and set raise amount 
+print(f"employee count = {employee_info.employee_count}") 
+print(f"before employee standard raise amount {employee_info.raise_amount}")  
 print()
 
+#employee list and initialization
 emp_1=employee_info("Fred", "Dejrangsi", 66000)
 emp_2=employee_info("Matilda", "May", 100000)
 
-print(f"pay before raise {emp_1.pay}")
-emp_1.apply_raise()
-print(f"pay after a raise {emp_1.pay}")
-
-
-print()
-print(f"employee count = {employee_info.employee_count}") 
-print()
-
-#used to find more information on instance or class variables
+#peak into class attritbutes
 print("--------------------")
 print(emp_1.__dict__)
+print(emp_2.__dict__)
 print("--------------------")
-print(employee_info.__dict__)
 
-#trying to use method to make a raise to emp_1
-# print(emp_1.pay)
-# emp_1.apply_raise()
-# print(emp_1.pay)
+#spacing
+print(""" 
+      BEFORE RAISE AMOUNT""")
+
+#employee 1 pre raise
+print(f"employee 1 raise amount {emp_1.raise_amount}")
+print(f"pay before raise {emp_1.pay}")
+
+#employee 2 pre raise
+print(f"employee 2 raise amount {emp_2.raise_amount}")
+print(f"pay before raise {emp_2.pay}")
 
 
-            
+#increase pay
+emp_1.raise_amount = 1.06
+employee_info.set_raise_amt(1.07)
+
+
+#spacing
+print("""
+      AFTER RAISE AMOUNT""")
+
+#employee 1 after raise
+print(f"employee 1 raise amount after raise {emp_1.raise_amount}")
+print(f"pay after raise {emp_1.pay}")
+
+#employee 2 after raise
+print(f"employee 2 raise amount after raise {emp_2.raise_amount}")
+print(f"pay after raise {emp_2.pay}")
+
+#spacing
+print("""
+      """)
+
+#peak into class attritbutes
+print("--------------------")
+print(emp_1.__dict__)
+print(emp_2.__dict__)
+print("--------------------")
+
+#no. of emplyoees after being instantiated
+print()
+print(f"employee count = {employee_info.employee_count}") 
